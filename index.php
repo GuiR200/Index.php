9e978eeb0bdcb966b57dc0930dc0828c2246a583
unknown <you@example.com>
2022-04-04 19:02:01 -0300

    xampp

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2</title>
+</head>
+<body>
+    <main>
+        <header>
+            <h1>Aula 02</h1>
+            <nav>Menu</nav>
+        </header>
+        
+        <section id="dadosPessoais">
+            <h2>Dados Pessoais</h2>
+            <p><span class="titulo">Nome:</span> Rafael Estrela</p>
+            <p><span class="titulo">Idade:</span> 25 anos</p>
+            <p><span class="titulo">Cidade onde mora:</span> Porto Alegre/RS</p>
+        </section>
+
+        <aside>
+            Barra Lateral
+            <article>Artigo 1</article>
+            <article>Artigo 2</article>
+        </aside>
+
+        <footer>Rodape</footer>
+    </main>
+</body>
+</html>
